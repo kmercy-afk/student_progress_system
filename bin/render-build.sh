2383315d69118ca3140d481876b9634dbc699a2a
Mercy Wanji <kmercyafk@gmail.com>
2026-06-30 17:48:24 +0300

    Fix bundler command for Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,9 @@ export RUBYOPT=-rlogger
 export NODE_OPTIONS=--openssl-legacy-provider
 
 gem install bundler -v 2.4.22
-bundle _2.4.22_ install
+bundle update --bundler
+bundle install
 yarn install --ignore-engines
-bundle _2.4.22_ exec rails assets:precompile
-bundle _2.4.22_ exec rails db:migrate
-bundle _2.4.22_ exec rails db:seed
+bundle exec rails assets:precompile
+bundle exec rails db:migrate
+bundle exec rails db:seed
