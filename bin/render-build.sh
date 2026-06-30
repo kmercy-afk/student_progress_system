ae7c892fe00f5fe5691a52b7630f33c082b8dd26
Mercy Wanji <kmercyafk@gmail.com>
2026-06-30 17:40:38 +0300

    Use newer bundler on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,9 @@ set -o errexit
 export RUBYOPT=-rlogger
 export NODE_OPTIONS=--openssl-legacy-provider
 
-bundle install
+gem install bundler -v 2.4.22
+bundle _2.4.22_ install
 yarn install --ignore-engines
-bundle exec rails assets:precompile
-bundle exec rails db:migrate
-bundle exec rails db:seed
+bundle _2.4.22_ exec rails assets:precompile
+bundle _2.4.22_ exec rails db:migrate
+bundle _2.4.22_ exec rails db:seed
